1bdc5a66ba5dfd5f7270063f1c3a397f62fe37f6
Gülseren Zamir <86431567+gulseren27@users.noreply.github.com>
2024-02-14 23:46:13 +0300

    scrapingG2 ekleme

--- scrapingG.py
+++ scrapingG.py
@@ -63,7 +63,7 @@ for section in sections:
 #deneme2
 
 # Yorumları içeren etiketleri bul 
-reviews = product_soup.find_all("div", class_="reviews")
+#reviews = product_soup.find_all("div", class_="reviews")
 
 # Eğer en az bir yorum bulunuyorsa devam et
 if reviews:
